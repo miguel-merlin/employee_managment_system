a187c8d7cda276d02a516397d9659f7a54d48412
miguel-merlin <mmerlin@stevens.edu>
2024-01-20 13:45:32 -0500

    feat: error handling EmployeeServiceImpl

--- src/main/java/com/gamma_computing/employees/service/EmployeeServiceImpl.java
+++ src/main/java/com/gamma_computing/employees/service/EmployeeServiceImpl.java
@@ -3,10 +3,12 @@ package com.gamma_computing.employees.service;
 import com.gamma_computing.employees.model.Employee;
 import com.gamma_computing.employees.repository.EmployeeRepository;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl  implements EmployeeService {
@@ -15,26 +17,52 @@ public class EmployeeServiceImpl  implements EmployeeService {
 
     @Override
     public List<Employee> getAllEmployees() {
-        return employeeRepository.findAll();
+        try {
+            return employeeRepository.findAll();
+        } catch (Exception e) {
+            throw new RuntimeException("Error retrieving employees: ", e);
+        }
     }
 
     @Override
     public Employee getEmployee(Long employeeId) {
-        return employeeRepository.findById(employeeId).get();
+        try {
+            Optional<Employee> employee = employeeRepository.findById(employeeId);
+            if (!employee.isPresent()) {
+                throw new EntityNotFoundException("Employee with id " + employeeId + " not found");
+            }
+            return employee.get();
+        } catch (EntityNotFoundException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException("Error retrieving employee with id: " + employeeId.toString(), e);
+        }
     }
 
     @Override
     public Employee createEmployee(Employee employee) {
-        return employeeRepository.save(employee);
+        try {
+            return employeeRepository.save(employee);
+        } catch (Exception e) {
+            throw new RuntimeException("Error creating creating employee", e);
+        }
     }
 
     @Override
     public Employee updateEmployee(Employee employee) {
-        return employeeRepository.saveAndFlush(employee);
+        try {
+            return employeeRepository.saveAndFlush(employee);
+        } catch (Exception e) {
+            throw new RuntimeException("Error updating employee");
+        }
     }
 
     @Override
     public void deleteEmployee(Long employeeId) {
-        employeeRepository.deleteById(employeeId);
+        try {
+            employeeRepository.deleteById(employeeId);
+        } catch (Exception e) {
+            throw  new RuntimeException("Error deleting employee");
+        }
     }
 }
